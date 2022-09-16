e59c5fde6734dc8667e7a2b723fd8854be893453
ChocolateSocrotes <52463084+DunkanGibson@users.noreply.github.com>
2022-09-16 17:45:03 -0400

    Finished project

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const key = document.querySelectorAll("div.key");
+const clap = document.getElementById("clap")
+const hihat = document.getElementById("hihat")
+const kick = document.getElementById("kick")
+const openhat = document.getElementById("openhat")
+const boom = document.getElementById("boom")
+const ride = document.getElementById("ride")
+const snare = document.getElementById("snare")
+const tom = document.getElementById("tom")
+const tink = document.getElementById("tink")
 
 const keyTransformStylesOnPress = `
     transform: scale(1.1);
     outline: solid #ffc600 3px;
+    box-shadow: 0 0 1rem #ffc600;
 `
 
 const keyTransformStylesOnRelease = `
@@ -12,10 +22,41 @@ const keyTransformStylesOnRelease = `
 
 addEventListener('keydown', e => {
     
-  if(e.key === "a" || e.key === "s"){
-    key.forEach((keyBoardItem) => {
-        keyBoardItem.style.cssText = keyTransformStylesOnPress;
-      }); 
+  if(e.key === "a"){
+    clap.style.cssText = keyTransformStylesOnPress;
+    playSound("clap")
+    }
+  else if(e.key === "s"){
+    hihat.style.cssText = keyTransformStylesOnPress;
+    playSound("hihat")
+    }
+  else if(e.key === "d"){
+    kick.style.cssText = keyTransformStylesOnPress;
+    playSound("kick")
+    }
+  else if(e.key === "f"){
+    openhat.style.cssText = keyTransformStylesOnPress;
+    playSound("openhat")
+    }
+  else if(e.key === "g"){
+    boom.style.cssText = keyTransformStylesOnPress;
+    playSound("boom")
+    }
+  else if(e.key === "h"){
+    ride.style.cssText = keyTransformStylesOnPress;
+    playSound("ride")
+    }
+  else if(e.key === "j"){
+    snare.style.cssText = keyTransformStylesOnPress;
+    playSound("snare")
+    }
+  else if(e.key === "k"){
+    tom.style.cssText = keyTransformStylesOnPress;
+    playSound("tom")
+    }
+  else if(e.key === "l"){
+    tink.style.cssText = keyTransformStylesOnPress;
+    playSound("tink")
     }
 });
 
@@ -25,13 +66,10 @@ addEventListener('keyup', e => {
   }); 
 });
 
+function playSound(soundToPlay){
+var musicPath = `sounds\\${soundToPlay}.wav`;    
+var music = new Audio(musicPath);
+music.play();
+}
 
-// function transformKey(){
-    
-// }
 
-// function playSound(){
-//     if (event.code == 'KeyZ' && (event.ctrlKey || event.metaKey)) {
-//         alert('Undo!')
-//       }
-// }
